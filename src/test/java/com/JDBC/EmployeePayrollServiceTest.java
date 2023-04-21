2e3cc1989e4e1c67544715c77aa9c70ea81bbba7
Vishruti Patil <vishruti2908@gmail.com>
2023-04-21 08:33:38 +0530

    Able to update salary for employee name 'DEF' from table

--- src/test/java/com/JDBC/EmployeePayrollServiceTest.java
+++ src/test/java/com/JDBC/EmployeePayrollServiceTest.java
@@ -1,14 +1,21 @@
 package com.JDBC;
 import org.junit.Test;
-import java.util.List;
 import static org.junit.Assert.*;
 
 public class EmployeePayrollServiceTest {
     @Test
     public void testGetEmployeePayrollList() {
         EmployeePayrollService service = new EmployeePayrollService();
-        List<EmployeePayroll> employeePayrollList = service.getEmployeePayrollList();
-        assertNotNull(employeePayrollList);
-        assertFalse(employeePayrollList.isEmpty());
+        assertFalse(service.getEmployeePayrollList().isEmpty());
+    }
+
+    @Test
+    public void testUpdateEmployeeSalary() {
+        EmployeePayrollService service = new EmployeePayrollService();
+        String name = "DEF";
+        double newSalary = 3000000.00;
+        service.updateEmployeeSalary(name, newSalary);
+        EmployeePayroll employeePayroll = service.getEmployeePayrollByName(name);
+        assertEquals(newSalary, employeePayroll.getSalary(), 0.0);
     }
 }
