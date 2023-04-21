549bfeeadcc39ec3de7cd1699db3fee1606f4c9e
Vishruti Patil <vishruti2908@gmail.com>
2023-04-21 12:44:03 +0530

    Able to find sum, average, min and max salary by Gender

--- src/test/java/com/JDBC/EmployeePayrollServiceTest.java
+++ src/test/java/com/JDBC/EmployeePayrollServiceTest.java
@@ -1,23 +1,46 @@
 package com.JDBC;
-import static org.junit.Assert.assertEquals;
-import java.time.LocalDate;
-import java.util.List;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeePayrollServiceTest {
-    private static EmployeePayrollService service;
 
-    @BeforeClass
-    public static void setUp() {
-        service = new EmployeePayrollService();
+    @Test
+    void testGetSumOfSalaryByGender() {
+        EmployeePayrollService service = new EmployeePayrollService();
+        double expected = 50000.0;
+        double actual = service.getSumOfSalaryByGender("M");
+        assertEquals(expected, actual, 0.001);
     }
 
     @Test
-    public void givenDateRange_WhenRetrievingEmployeePayrollList_ThenReturnCorrectList() {
-        LocalDate startDate = LocalDate.of(2022, 1, 1);
-        LocalDate endDate = LocalDate.of(2022, 12, 31);
-        List<EmployeePayroll> payrollList = service.getEmployeePayrollListByDateRange(startDate, endDate);
-        assertEquals(3, payrollList.size());
+    void testGetAverageSalaryByGender() {
+        EmployeePayrollService service = new EmployeePayrollService();
+        double expected = 50000.0;
+        double actual = service.getAverageSalaryByGender("M");
+        assertEquals(expected, actual, 0.001);
+    }
+
+    @Test
+    void testGetMinSalaryByGender() {
+        EmployeePayrollService service = new EmployeePayrollService();
+        double expected = 50000.0;
+        double actual = service.getMinSalaryByGender("M");
+        assertEquals(expected, actual, 0.001);
+    }
+
+    @Test
+    void testGetMaxSalaryByGender() {
+        EmployeePayrollService service = new EmployeePayrollService();
+        double expected = 50000.0;
+        double actual = service.getMaxSalaryByGender("M");
+        assertEquals(expected, actual, 0.001);
+    }
+
+    @Test
+    void testGetCountByGender() {
+        EmployeePayrollService service = new EmployeePayrollService();
+        int expected = 1;
+        int actual = service.getCountByGender("M");
+        assertEquals(expected, actual);
     }
 }
